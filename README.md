7fb2b0628c2116b6316fdf50f6501a991b5d8e00
Michael Wooten <mwootendev@gmail.com>
2015-08-02 00:07:33 +0000

    1 - Adding initial project skeleton.

---
+++ README.md
@@ -0,0 +1,69 @@
+# Slush Polymer-seed-element [![Build Status](https://secure.travis-ci.org/mwootendev/slush-polymer-seed-element.png?branch=master)](https://travis-ci.org/mwootendev/slush-polymer-seed-element) [![NPM version](https://badge-me.herokuapp.com/api/npm/slush-polymer-seed-element.png)](http://badges.enytc.com/for/npm/slush-polymer-seed-element)
+
+> A slush generator for a Polymer element project based on PolymerElements/seed-element.
+
+
+## Getting Started
+
+Install `slush-polymer-seed-element` globally:
+
+```bash
+$ npm install -g slush-polymer-seed-element
+```
+
+### Usage
+
+Create a new folder for your project:
+
+```bash
+$ mkdir my-slush-polymer-seed-element
+```
+
+Run the generator from within the new folder:
+
+```bash
+$ cd my-slush-polymer-seed-element && slush polymer-seed-element
+```
+
+## Getting To Know Slush
+
+Slush is a tool that uses Gulp for project scaffolding.
+
+Slush does not contain anything "out of the box", except the ability to locate installed slush generators and to run them with liftoff.
+
+To find out more about Slush, check out the [documentation](https://github.com/slushjs/slush).
+
+## Contributing
+
+See the [CONTRIBUTING Guidelines](https://github.com/mwootendev/slush-polymer-seed-element/blob/master/CONTRIBUTING.md)
+
+## Support
+If you have any problem or suggestion please open an issue [here](https://github.com/mwootendev/slush-polymer-seed-element/issues).
+
+## License 
+
+The MIT License
+
+Copyright (c) 2015, Michael Wooten
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ slushfile.js
@@ -0,0 +1,98 @@
+/*
+ * slush-polymer-seed-element
+ * https://github.com/mwootendev/slush-polymer-seed-element
+ *
+ * Copyright (c) 2015, Michael Wooten
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var gulp = require('gulp'),
+    install = require('gulp-install'),
+    conflict = require('gulp-conflict'),
+    template = require('gulp-template'),
+    rename = require('gulp-rename'),
+    _ = require('underscore.string'),
+    inquirer = require('inquirer'),
+    path = require('path');
+
+function format(string) {
+    var username = string.toLowerCase();
+    return username.replace(/\s/g, '');
+}
+
+var defaults = (function () {
+    var workingDirName = path.basename(process.cwd()),
+      homeDir, osUserName, configFile, user;
+
+    if (process.platform === 'win32') {
+        homeDir = process.env.USERPROFILE;
+        osUserName = process.env.USERNAME || path.basename(homeDir).toLowerCase();
+    }
+    else {
+        homeDir = process.env.HOME || process.env.HOMEPATH;
+        osUserName = homeDir && homeDir.split('/').pop() || 'root';
+    }
+
+    configFile = path.join(homeDir, '.gitconfig');
+    user = {};
+
+    if (require('fs').existsSync(configFile)) {
+        user = require('iniparser').parseSync(configFile).user;
+    }
+
+    return {
+        userName: osUserName || format(user.name || ''),
+        authorName: user.name || '',
+        authorEmail: user.email || ''
+    };
+})();
+
+gulp.task('default', function (done) {
+    var prompts = [{
+        name: 'elementName',
+        message: 'What is the name of your element (ex: seed-element)?',
+        default: 'seed-element'
+    }, {
+        name: 'elementDescription',
+        message: 'What is the description of your element?'
+    }, {
+        name: 'authorName',
+        message: 'Who is the author of this element?',
+        default: defaults.authorName
+    }, {
+        name: 'authorEmail',
+        message: 'What is the author\'s email address?',
+        default: defaults.authorEmail
+    }, {
+        name: 'githubUsername',
+        message: 'What is the author\'s github username?',
+        default: defaults.userName
+    }, {
+        type: 'confirm',
+        name: 'moveon',
+        message: 'Continue?'
+    }];
+    //Ask
+    inquirer.prompt(prompts,
+        function (answers) {
+            if (!answers.moveon) {
+                return done();
+            }
+            answers.appNameSlug = _.slugify(answers.appName);
+            gulp.src(__dirname + '/templates/**')
+                .pipe(template(answers))
+                .pipe(rename(function (file) {
+                    if (file.basename[0] === '_') {
+                        file.basename = '.' + file.basename.slice(1);
+                    }
+                }))
+                .pipe(conflict('./'))
+                .pipe(gulp.dest('./'))
+                .pipe(install())
+                .on('end', function () {
+                    done();
+                });
+        });
+});
